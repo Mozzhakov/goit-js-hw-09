48bad957f0448d8c2307d742da62bfa59569bc1a
Mozzhakov <imozzhakov@icloud.com>
2022-12-02 13:31:46 +0200

    add color-picker

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,25 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const INTERVAL = 1000;
+let changeColorInterval = null;
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartBtnClick() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  changeColorInterval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, INTERVAL);
+  startBtn.disabled = true;
+}
+
+function onStopBtnClick() {
+  clearInterval(changeColorInterval);
+  document.body.style.backgroundColor = '#FFFFFF';
+  startBtn.disabled = false;
+}
